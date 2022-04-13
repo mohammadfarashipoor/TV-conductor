1774b07d19426e66080015e3203de3c7a81a58a1
Mohammad <mohammad1f2f@gmail.com>
2022-04-13 09:56:37 +0430

    active button

--- src/components/WeekBtn.js
+++ src/components/WeekBtn.js
@@ -1,38 +1,39 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Button from './Button';
 
 
 function WeekBtn() {
+  const [button,setButton]=useState(1);
+  const week = [
+    { id:1 ,
+      day:"شنبه"},
+    { id: 2,
+      day:"یک شنبه"},
+    { id:3 ,
+      day:"دوشنبه"},
+    { id:4 ,
+      day:"سه شنبه"},
+    { id:5 ,
+      day:"چهارشنبه"},
+    { id:6 ,
+      day:"پنج شنبه"},
+    { id:7 ,
+      day:"جمعه"}
+  ];
+  const activeBtn=(item)=>{
+    if(item===button){
+      return "active";
+    }
+  }
   return (
     <div className="d-flex align-items-center flex-wrap p-3 
-    "><div className="m-1">
-    <Button type="secondary" textbutton="شنبه">
-    </Button>
-    </div>
-    <div className="m-1">
-    <Button type="secondary" textbutton="یکشنبه">
-    </Button>
-    </div>
-    <div className="m-1">
-    <Button type="secondary" textbutton="دوشنبه">
-    </Button>
-    </div>
-    <div className="m-1">
-    <Button type="secondary" textbutton="سه شنبه">
-    </Button>
-    </div>
-    <div className="m-1">
-    <Button type="secondary" textbutton="چهارشنبه">
-    </Button>
-    </div>
-    <div className="m-1">
-    <Button type="secondary" textbutton="پنج شنبه">
-    </Button>
-    </div>            
-    <div className="m-1">
-    <Button type="secondary" textbutton="جمعه">
-    </Button>
-    </div>
+    ">
+    {week.map((item)=>(
+      <div className="m-1" key={item.id}  onClick={() =>{setButton(item.id)}} id={activeBtn(item.id)}>
+        <Button type="secondary" textbutton={item.day}>
+        </Button>
+      </div>
+    ))}
     </div>
   )
 }
