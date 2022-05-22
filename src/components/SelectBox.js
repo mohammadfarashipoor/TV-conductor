8d905820e33cfbd24d01b111224540a6c32ef62f
Mohammad <mohammad1f2f@gmail.com>
2022-05-22 17:54:26 +0430

    fix bug onclick

--- src/components/SelectBox.js
+++ src/components/SelectBox.js
@@ -3,13 +3,13 @@ import {ClaySelect} from '@clayui/form';
 const SelectBox = (props) => {
     const options = props.options;
     return (
-      <ClaySelect defaultValue={props.val} aria-label="Select Label" id="mySelectId">
+      <ClaySelect defaultValue={props.val} aria-label="Select Label" id="mySelectId"onChange={props.onClick}
+      >
         {options.map(item => (
           <ClaySelect.Option
             key={item.value}
             label={item.label}
             value={item.value}
-            onClick={props.onClick}
           />
         ))}
       </ClaySelect>
